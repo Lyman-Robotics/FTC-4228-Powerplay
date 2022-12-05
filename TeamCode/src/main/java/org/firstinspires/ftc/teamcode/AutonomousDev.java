145ab26d1d4a72618a050064cf90229ce272388f
Loki7261 <100491484+Loki7261@users.noreply.github.com>
2022-12-04 19:05:38 -0500

    Competition 3 Changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDev.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Autonomous Dev", group = "Autonomous Dev")
+public class AutonomousDev extends LinearOpMode {
+  @Override
+  public void runOpMode() {
+    // Initialize the hardware variables.
+    RobotClass robot = new RobotClass(hardwareMap, false);
+
+    // ! Runs upon initialization
+    telemetry.addData("Status", "Initialized");
+    telemetry.update();
+
+    robot.BRDrive.setDirection(DcMotor.Direction.FORWARD);
+    robot.FLDrive.setDirection(DcMotor.Direction.REVERSE);
+    robot.FRDrive.setDirection(DcMotor.Direction.REVERSE);
+    robot.BLDrive.setDirection(DcMotor.Direction.FORWARD);
+
+    boolean canAWork = true;
+    boolean canBWork = false;
+    int segmentAmount = 0;
+    boolean running = false;
+    ElapsedTime segmentTime = new ElapsedTime();
+    String segmentEnd = "";
+
+    // ! Runs until the end of the match after play is pressed
+    waitForStart();
+    robot.timeElapsed.reset();
+
+    while (opModeIsActive()) {
+      if (gamepad1.a && canAWork) {
+        canAWork = false;
+        canBWork = true;
+        running = true;
+
+        segmentAmount++;
+        segmentTime.reset();
+      }
+
+      if (gamepad1.b && canBWork) {
+        canAWork = true;
+        canBWork = false;
+        running = false;
+
+        segmentEnd = segmentTime.toString();
+        robot.stopDrive();
+      }
+
+      if (running) {
+        if (gamepad1.left_stick_y < 0) {
+          robot.setDrivePower(0.25, 0.25, 0.25, 0.25);
+        } else if (gamepad1.left_stick_y > 0) {
+          robot.setDrivePower(-0.25, -0.25, -0.25, -0.25);
+        } else if (gamepad1.left_stick_x > 0) {
+          robot.omnidrive(0.5, (Math.PI / 2), 0);
+        } else if (gamepad1.left_stick_x < 0) {
+          robot.omnidrive(-0.5, (Math.PI / 2), 0);
+        } else {
+          robot.setDrivePower(0, 0, 0, 0);
+        }
+      }
+
+      telemetry.addData("Segment " + segmentAmount + " time", segmentTime.toString());
+      telemetry.addData("Running", running);
+      telemetry.addData("Segment End", segmentEnd);
+      telemetry.update();
+    }
+  }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRight.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//all ofg this is copy an dpasted from visionTest
+// import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+// import org.firstinspires.ftc.teamcode.common.powerplay.SleeveDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous(name = "Red Right", group = "Autonomous")
+// @Disabled
+public class RedRight extends LinearOpMode {
+  @Override
+  public void runOpMode() {
+    // Initialize the hardware variables.
+    RobotClass robot = new RobotClass(hardwareMap, true);
+
+    // ! Runs upon initialization
+    telemetry.addData("Status", "Initialized");
+    telemetry.update();
+
+    robot.BRDrive.setDirection(DcMotor.Direction.FORWARD);
+    robot.FLDrive.setDirection(DcMotor.Direction.REVERSE);
+    robot.FRDrive.setDirection(DcMotor.Direction.REVERSE);
+    robot.BLDrive.setDirection(DcMotor.Direction.FORWARD);
+
+    // TODO Open CV
+    while (!isStarted()) {
+      telemetry.addData("Position", robot.sleeveDetection.getPosition());
+      telemetry.addData("PRELOAD CONE", "PRELOAD IT DUMBASS");
+      telemetry.update();
+      robot.position = robot.sleeveDetection.getPosition();
+    }
+
+    // ! Runs until the end of the match after play is pressed
+    waitForStart();
+    robot.timeElapsed.reset();
+
+    while (opModeIsActive()) {
+      // ! New Stuff
+      robot.ClawServo.setPosition(robot.servoClosePos);
+      realSleep(1000,"Close Servo with cone",robot);
+      robot.SlideMotor.setPower(robot.slidePowerUp);
+      realSleep(350,"Slide up with cone",robot);
+      robot.SlideMotor.setPower(0);
+      realSleep(500, "Wait to move", robot);
+      robot.setDrivePower(.25,.25,.25,.25);
+      realSleep(1560,"Move to signal cone",robot);
+      robot.stopDrive();
+      realSleep(500, "Wait to show cone", robot);
+      
+      if (robot.position.equals("Left")) {
+        // ! Bad
+        robot.stopDrive();
+        robot.omnidrive(0.5, robot.omniLeftVal, 0);
+        realSleep(1300,"omni to pole",robot);
+        robot.stopDrive();
+      } else if (robot.position.equals("Center")) {
+        // ** Good
+        robot.stopDrive();
+      } else if (robot.position.equals("Right")) {
+        // ! Bad
+        robot.stopDrive();
+        robot.omnidrive(0.5, robot.omniRightVal, 0);
+        realSleep(1300,"omni to pole",robot);
+        robot.stopDrive();
+      }
+
+      realSleep(9999999, "Done", robot);
+    }
+  }
+
+  public void realSleep(int n, String customAdd, RobotClass robot) { // better sleep method, dont use other crappy
+                                                                     // stuffs
+    telemetry.addData("Status", customAdd);
+    telemetry.addData("Claw Servo Position", robot.ClawServo.getPosition());
+    telemetry.addData("Slide Touch Sensor", !(robot.SlideTouchSensor.getState()));
+    telemetry.addData("Elapsed Time", robot.timeElapsed.toString());
+    telemetry.addData("Cone Pos", robot.position);
+    telemetry.update();
+
+    sleep(n);
+  }
+}
